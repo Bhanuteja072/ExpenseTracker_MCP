4b0fc1828ae7dbd3b4b730d36cec6598eca7e647
Bhanteja072 <bhanutejapusarla@gmail.com>
2026-01-29 19:41:37 +0530

    Sample dice code to check

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from fastmcp import FastMCP
 
 mcp = FastMCP(name="expensetracker")
 
+@mcp.tool
+def roll_dice(n_dice : int = 1) -> list[int]:
+    """Roll a specified number of six-sided dice and return the results."""
+    return [random.randint(1, 6) for _ in range(n_dice)]
+
+@mcp.tool
+def add_two_num(a: int, b: int) -> int:
+    """Add two numbers and return the result."""
+    return a + b
+
+if __name__ == "__main__":
+    mcp.run()
